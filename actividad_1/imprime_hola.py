4bf2f61fb740f810e904bc6600352fa926f24bd1
Claudia González Scanu <93352638+ClaudiaGonzalezScanu@users.noreply.github.com>
2021-11-05 18:35:06 -0300

    Delete imprime_hola.py
    duplicado

--- actividad_1/imprime_hola.py
+++
@@ -1,6 +0,0 @@
-#este es un programa, que pregunta tu nombre, apellido, edad, direccion, fecha de nacimiento
-#correo electronico y telefono. ademas debe sumar edad y agno de nacimiento
-print ("Por favor, dime tu nombre")
-nombre_persona = input()
-print(f"es un gusto conocerte, {nombre_persona}")
-
