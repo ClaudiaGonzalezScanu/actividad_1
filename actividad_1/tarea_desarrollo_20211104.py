33c6078b6d9d957e0238ec8406d81f1e8a6abd16
Claudia González Scanu <93352638+ClaudiaGonzalezScanu@users.noreply.github.com>
2021-11-05 18:35:33 -0300

    Delete tarea_desarrollo_20211104.py
    duplicado

--- actividad_1/tarea_desarrollo_20211104.py
+++
@@ -1,9 +0,0 @@
-#Hacer dos función, 1 solicitando mis datos personales, inicializar dos variables numéricas
-# inicializadas las variables numéricas que sean de parámetros de entreda de otra función que realice
-#las 4 operaciones aritméticas (+,-,*,/), haciendo uso de condicionales uso de operadores lógicos(< > !=, = ),
-#posteriormente, tiene que retornar e invocar ambas funciones print(i)
-elementos_formulario = ["nombre", "apellido", "telefono","fecha de nacimiento","correo"]
-datos_formulario = [input(ingrese nombre: )]
-
-
-
