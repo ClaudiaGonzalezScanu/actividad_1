f4269f4e110a1056e3f1981da60c8a59130ffb57
Claudia González Scanu <93352638+ClaudiaGonzalezScanu@users.noreply.github.com>
2021-11-05 17:58:13 -0300

    actualizacion20211105
    organizando github

---
+++ actividad_1/imprime_hola.py
@@ -0,0 +1,6 @@
+#este es un programa, que pregunta tu nombre, apellido, edad, direccion, fecha de nacimiento
+#correo electronico y telefono. ademas debe sumar edad y agno de nacimiento
+print ("Por favor, dime tu nombre")
+nombre_persona = input()
+print(f"es un gusto conocerte, {nombre_persona}")
+
---
+++ actividad_1/tarea20211103.py
@@ -0,0 +1,11 @@
+#Hacer dos función, 1 solicitando mis datos personales, inicializar dos variables numéricas
+# inicializadas las variables numéricas que sean de parámetros de entreda de otra función que realice
+#las 4 operaciones aritméticas (+,-,*,/), haciendo uso de condicionales uso de operadores lógicos(< > !=, = ),
+#posteriormente, tiene que retornar e invocar ambas funciones print(i)
+def datos_personales(nombre="Claudia", apellido="Gonzalez"):
+    informacion_completa = nombre, apellido
+    print(informacion_completa)
+print(datos_personales())
+
+
+
